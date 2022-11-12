c8479140be4e82c56cfa0361272bed977b7a6932
ABDULLAH <abdullah@swishtag.com>
2022-11-12 23:15:00 +0500

    changes

--- application/views/user_template/home_2.php
+++ application/views/user_template/home_2.php
@@ -735,130 +735,25 @@
         </div>
       </div>
     </div>
-  </div> 
-  <!--   Core JS Files   -->
+  </div>
   <script src="newAssets/js/core/popper.min.js"></script>
   <script src="newAssets/js/core/bootstrap.min.js"></script>
   <script src="newAssets/js/plugins/perfect-scrollbar.min.js"></script>
   <script src="newAssets/js/plugins/smooth-scrollbar.min.js"></script>
-  <script src="newAssets/js/plugins/choices.min.js"></script>
-  <!-- Kanban scripts -->
-  <script src="newAssets/js/plugins/dragula/dragula.min.js"></script>
-  <script src="newAssets/js/plugins/jkanban/jkanban.js"></script>
-  <script src="newAssets/js/plugins/countup.min.js"></script>
-  <script src="newAssets/js/plugins/chartjs.min.js"></script>
-  <script src="newAssets/js/plugins/round-slider.min.js"></script>
-  <script>
-    // Rounded slider
-    const setValue = function(value, active) {
-      document.querySelectorAll("round-slider").forEach(function(el) {
-        if (el.value === undefined) return;
-        el.value = value;
-      });
-      const span = document.querySelector("#value");
-      span.innerHTML = value;
-      if (active)
-        span.style.color = 'red';
-      else
-        span.style.color = 'black';
-    }
-
-    document.querySelectorAll("round-slider").forEach(function(el) {
-      el.addEventListener('value-changed', function(ev) {
-        if (ev.detail.value !== undefined)
-          setValue(ev.detail.value, false);
-        else if (ev.detail.low !== undefined)
-          setLow(ev.detail.low, false);
-        else if (ev.detail.high !== undefined)
-          setHigh(ev.detail.high, false);
-      });
-
-      el.addEventListener('value-changing', function(ev) {
-        if (ev.detail.value !== undefined)
-          setValue(ev.detail.value, true);
-        else if (ev.detail.low !== undefined)
-          setLow(ev.detail.low, true);
-        else if (ev.detail.high !== undefined)
-          setHigh(ev.detail.high, true);
-      });
-    });
-
-    // Count To
-    if (document.getElementById('status1')) {
-      const countUp = new CountUp('status1', document.getElementById("status1").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-    if (document.getElementById('status2')) {
-      const countUp = new CountUp('status2', document.getElementById("status2").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-    if (document.getElementById('status3')) {
-      const countUp = new CountUp('status3', document.getElementById("status3").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-    if (document.getElementById('status4')) {
-      const countUp = new CountUp('status4', document.getElementById("status4").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-    if (document.getElementById('status5')) {
-      const countUp = new CountUp('status5', document.getElementById("status5").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-    if (document.getElementById('status6')) {
-      const countUp = new CountUp('status6', document.getElementById("status6").getAttribute("countTo"));
-      if (!countUp.error) {
-        countUp.start();
-      } else {
-        console.error(countUp.error);
-      }
-    }
-  </script>
-  <script>
-    var win = navigator.platform.indexOf('Win') > -1;
-    if (win && document.querySelector('#sidenav-scrollbar')) {
-      var options = {
-        damping: '0.5'
-      }
-      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
-    }
-  </script>
-  <!-- Github buttons -->
-  <script async defer src="https://buttons.github.io/buttons.js"></script>
-  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
   <script src="newAssets/js/soft-ui-dashboard.min.js?v=1.1.0"></script>
 	<!-- new code end -->
 	<!-- Start  scripts -->
 	<!-- <script type="text/javascript" src="//widget.trustpilot.com/bootstrap/v5/tp.widget.bootstrap.min.js" async></script> -->
-	<!-- <script src="assets/js/jquery-1.12.4.js"></script> -->
-	<!-- <script src="assets/js/circle-progress-1.2.2.min.js"></script> -->
-	<!-- <script src="assets/js/jquery.slicknav.min.js"></script> -->
-	<!-- <script src="assets/js/smooth-scroll.polyfills.min.js"></script> -->
-	<!-- <script src="assets/js/masonry.min.js"></script> -->
-	<!-- <script src="assets/js/jquery.slicknav.min.js"></script> -->
-	<!-- <script src="assets/js/owl.carousel.min.js"></script> -->
-	<!-- <script src="assets/js/jquery.magnific-popup.min.js"></script> -->
+	<script src="assets/js/jquery-1.12.4.js"></script>
+	<script src="assets/js/circle-progress-1.2.2.min.js"></script>
+	<script src="assets/js/jquery.slicknav.min.js"></script>
+	<script src="assets/js/smooth-scroll.polyfills.min.js"></script>
+	<script src="assets/js/masonry.min.js"></script>
+	<script src="assets/js/jquery.slicknav.min.js"></script>
+	<script src="assets/js/owl.carousel.min.js"></script>
+	<script src="assets/js/jquery.magnific-popup.min.js"></script>
 	<!-- <script src="assets/js/bootstrap.min.js"></script> -->
-	<!-- <script src="assets/js/scrolls.js"></script> -->
+	<script src="assets/js/scrolls.js"></script>
 	<script src="assets/js/main.js"></script>
 	<script src="https://kit.fontawesome.com/3ffc819fd0.js" crossorigin="anonymous"></script>
 	<script type="text/javascript">
@@ -913,10 +808,10 @@
             }
         }
         // INJECT CSS
-        // var css = document.createElement("style");
-        // css.type = "text/css";
-        // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        // document.body.appendChild(css);
+        var css = document.createElement("style");
+        css.type = "text/css";
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
     };
 	</script>
 <!-- Global site tag (gtag.js) - Google Analytics -->
