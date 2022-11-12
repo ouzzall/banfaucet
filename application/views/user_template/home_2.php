d8750d19e73d6a12acec8dfa24d84672ddbcfd28
ABDULLAH <abdullah@swishtag.com>
2022-11-12 23:43:52 +0500

    changes

--- application/views/user_template/home_2.php
+++ application/views/user_template/home_2.php
@@ -39,6 +39,7 @@
 
 <body class="g-sidenav-show  bg-gray-100">
   <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
+    <hr class="horizontal dark mt-0">
   </aside>
   <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
     <!-- Navbar -->
