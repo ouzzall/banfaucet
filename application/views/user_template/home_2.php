5343b520e8b0007ac26fca424727d00818da82c5
ABDULLAH <abdullah@swishtag.com>
2022-11-13 00:23:11 +0500

    changes

--- application/views/user_template/home_2.php
+++ application/views/user_template/home_2.php
@@ -318,6 +318,12 @@
       </div>
       <hr class="horizontal dark my-1">
       <div class="card-body pt-sm-3 pt-0">
+      <div class="d-xl-block d-none">
+        <h6 class="mb-0">Light/Dark</h6>
+      </div>
+      <div class="form-check form-switch ps-0 d-xl-block d-none">
+        <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+      </div>
         <!-- Sidebar Backgrounds -->
         <!-- <hr class="horizontal dark mb-1 d-xl-block d-none">
         <div class="mt-2 d-xl-block d-none">
@@ -326,13 +332,6 @@
         <div class="form-check form-switch ps-0 d-xl-block d-none">
           <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarMinimize" onclick="navbarMinimize(this)">
         </div> -->
-        <hr class="horizontal dark mb-1 d-xl-block d-none">
-        <div class="mt-2 d-xl-block d-none">
-          <h6 class="mb-0">Light/Dark</h6>
-        </div>
-        <div class="form-check form-switch ps-0 d-xl-block d-none">
-          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
-        </div>
       </div>
     </div>
   </div>
