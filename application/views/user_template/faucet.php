1e3f0ca6cdcd458955bc8a2e9bdf16c51dc6b09e
massab <massab@swishtag.com>
2022-11-19 16:08:44 +0500

    changes

--- application/views/user_template/faucet.php
+++ application/views/user_template/faucet.php
@@ -32,48 +32,7 @@
     coinzilla_display.push(c_display_preferences);
 </script></center>
 <div class="row">
-<div class="col-lg-4 col-sm-6">
-              <div class="card  mb-4">
-                <div class="card-body p-3">
-                  <div class="row">
-                    <div class="col-8">
-                      <div class="numbers">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Claim Timer</p>
-                        <h5 class="font-weight-bolder mb-0">
-                          7:11
-                        </h5>
-                      </div>
-                    </div>
-                    <div class="col-4 text-end">
-                      <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                        <i class="fa-solid fa-stopwatch text-lg opacity-10"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="card ">
-                <div class="card-body p-3">
-                  <div class="row">
-                    <div class="col-8">
-                      <div class="numbers">
-                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Energy</p>
-                        <h5 class="font-weight-bolder mb-0">
-                          2
-                        </h5>
-                      </div>
-                    </div>
-                    <div class="col-4 text-end">
-                      <div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md">
-                        <!-- <i class="ni ni-world text-lg opacity-10" aria-hidden="true"></i> -->
-                        <i class="fa-solid fa-bolt text-lg opacity-10"></i>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-</div>
+
 <div class="col-lg-4 col-sm-6">
               <div class="card  mb-4">
                 <div class="card-body p-3">
