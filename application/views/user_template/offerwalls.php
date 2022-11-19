484f4bc346e65ef450562009fc3c872120bb161f
massab <massab@swishtag.com>
2022-11-19 12:56:16 +0500

    changes

--- application/views/user_template/offerwalls.php
+++ application/views/user_template/offerwalls.php
@@ -79,7 +79,34 @@
 <a href="https://banfaucet.com/new/offerwall/cpx" class="btn btn-primary btn-block text-dark" role="button" aria-pressed="true">View</a>
 </div>
 </div>
+<div class="col-md-6 col-xl-3" data-cat="survey">
+<div class="card">
+  <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
+    <a href="javascript:;" class="d-block">
+      <img src="./assets/img/kit/pro/anastasia.jpg" class="img-fluid border-radius-lg">
+    </a>
+  </div>
 
+  <div class="card-body pt-2">
+    <span class="text-gradient text-primary text-uppercase text-xs font-weight-bold my-2">House</span>
+    <a href="javascript:;" class="card-title h5 d-block text-darker">
+      Shared Coworking
+    </a>
+    <p class="card-description mb-4">
+      Use border utilities to quickly style the border and border-radius of an element. Great for images, buttons.
+    </p>
+    <div class="author align-items-center">
+      <img src="./assets/img/kit/pro/team-2.jpg" alt="..." class="avatar shadow">
+      <div class="name ps-3">
+        <span>Mathew Glock</span>
+        <div class="stats">
+          <small>Posted on 28 February</small>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
 <div class="col-md-6 col-xl-3" data-cat="survey">
 <div class="card">
 <div style="background-color: #B6C1E1;padding: 10px;border-top-left-radius: 10px;border-top-right-radius: 10px;transition:.3s;border:1px solid #939FC1">
