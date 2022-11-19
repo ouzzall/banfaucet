6643257f155c18df36aaa0e0d0a767d7be5feb92
massab <massab@swishtag.com>
2022-11-19 13:00:07 +0500

    changes

--- application/views/user_template/offerwalls.php
+++ application/views/user_template/offerwalls.php
@@ -83,27 +83,22 @@
 <div class="card">
   <div class="card-header p-0 mx-3 mt-3 position-relative z-index-1">
     <a href="javascript:;" class="d-block">
-      <img src="./assets/img/kit/pro/anastasia.jpg" class="img-fluid border-radius-lg">
+    <img style="max-width:350px;max-height:60px;" class="card-img-top img-fluid" src="https://banfaucet.com/new/assets/images/adscend.png" alt="Adscend Media">
     </a>
   </div>
 
   <div class="card-body pt-2">
-    <span class="text-gradient text-primary text-uppercase text-xs font-weight-bold my-2">House</span>
-    <a href="javascript:;" class="card-title h5 d-block text-darker">
-      Shared Coworking
-    </a>
-    <p class="card-description mb-4">
-      Use border utilities to quickly style the border and border-radius of an element. Great for images, buttons.
-    </p>
-    <div class="author align-items-center">
-      <img src="./assets/img/kit/pro/team-2.jpg" alt="..." class="avatar shadow">
-      <div class="name ps-3">
-        <span>Mathew Glock</span>
-        <div class="stats">
-          <small>Posted on 28 February</small>
-        </div>
+    <span class="badge bg-gradient-secondary">Secondary</span>
+    <span class="badge bg-gradient-secondary">Secondary</span>
+    <span class="badge bg-gradient-secondary">Secondary</span>
+    <div style="color:#2a3042cf;" data-toggle="tooltip" data-bs-postition="top" title="5 / 5 Rating">
+      <p style="text-align:center;color:#FAAD13">
+      <i class="fa-solid fa-star fa-xl"></i>
+        <i class="fa-solid fa-star fa-xl"></i>
+        <i class="fa-solid fa-star fa-xl"></i>
+        <i class="fa-solid fa-star fa-xl"></i>
+        <i class="fa-solid fa-star fa-xl"></i></p>
       </div>
-    </div>
   </div>
 </div>
 </div>
