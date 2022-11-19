8b48502c07612c9d476e09f89c1672b30e423b4c
massab <massab@swishtag.com>
2022-11-19 18:48:01 +0500

    changes

--- application/views/user_template/lottery.php
+++ application/views/user_template/lottery.php
@@ -1,6 +1,18 @@
-<div class="ads">
-    <?= $settings['lottery_top_ad'] ?>
+<div class="container-fluid py-4">
+<div class="row">
+<div class="col-12">
+          <div class="alert alert-warning alert-dismissible fade show text-center" role="alert">
+            <span class="alert-icon"><i class="fa-solid fa-circle-exclamation"></i></span>
+            <span class="alert-text">Earn a 15% bonus for every offer you complete from <a href="https://banfaucet.com/new/offerwall/timewall" style="color: #fff !important;font-weight:bold">Timewall!</a></span>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+            </button>
+        </div>
+    </div>
 </div>
+<!-- <div class="ads">
+    <?= $settings['lottery_top_ad'] ?>
+</div> -->
 <center><!-- Coinzilla Banner 728x90 -->
 <script async src="https://coinzillatag.com/lib/display.js"></script>
 <div class="coinzilla" data-zone="C-4766235297110347598"></div>
@@ -13,7 +25,61 @@
     coinzilla_display.push(c_display_preferences);
 </script></center>
 <div class="row">
-    <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
+<div class="col-lg-4 col-sm-6 mt-3">
+		<div class="card  mb-4">
+			<div class="card-body p-3">
+				<div class="row">
+					<div class="col-8">
+						<div class="numbers">
+							<p class="text-sm mb-0 text-capitalize font-weight-bold">Drawing In</p>
+							<h5 class="font-weight-bolder mb-0">
+                            <?= timespan(time(), $settings['lottery_date'], 2) ?>
+                        </h5> </div>
+					</div>
+					<div class="col-4 text-end">
+						<div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"> <i class="fa-solid fa-film text-lg opacity-10"></i> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="col-lg-4 col-sm-6 mt-3">
+		<div class="card  mb-4">
+			<div class="card-body p-3">
+				<div class="row">
+					<div class="col-8">
+						<div class="numbers">
+							<p class="text-sm mb-0 text-capitalize font-weight-bold">You Have</p>
+							<h5 class="font-weight-bolder mb-0">
+                            <?= $countLotteries ?> <?= $countLotteries > 0 ? 'tickets' : 'ticket' ?>
+                        </h5> </div>
+					</div>
+					<div class="col-4 text-end">
+						<div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"> <i class="fa-solid fa-ticket text-lg opacity-10"></i> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <div class="col-lg-4 col-sm-6 mt-3">
+		<div class="card  mb-4">
+			<div class="card-body p-3">
+				<div class="row">
+					<div class="col-8">
+						<div class="numbers">
+							<p class="text-sm mb-0 text-capitalize font-weight-bold">Current Jackpot</p>
+							<h5 class="font-weight-bolder mb-0">
+                            <?= currencyDisplay($current_reward, $settings) ?>
+                        </h5> </div>
+					</div>
+					<div class="col-4 text-end">
+						<div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"> <i class="fa-solid fa-clock text-lg opacity-10"></i> </div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+    <!-- <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
         <div class="card mini-stats-wid">
             <div class="card-body">
                 <div class="media">
@@ -30,8 +96,8 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
+    </div> -->
+    <!-- <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
         <div class="card mini-stats-wid">
             <div class="card-body">
                 <div class="media">
@@ -48,8 +114,8 @@
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
+    </div> -->
+    <!-- <div class="col-md-6 col-xl-4 mb-2 mb-xl-4">
         <div class="card mini-stats-wid">
             <div class="card-body">
                 <div class="media">
@@ -66,12 +132,12 @@
                 </div>
             </div>
         </div>
-    </div>
+    </div> -->
 </div>
 <center><span id="ct_cJd95UwrCma"></span></center><p>
 <center><span id="ct_c1c1RkyZBbe"></span></center><p>
 <div class="row">
-    <div class="col-12 col-md-8 col-lg-6 order-md-2 mb-4">
+    <div class="col-12 col-md-8 col-lg-6 order-md-2 mx-auto mb-4">
 
         <?php if (time() >= $settings['lottery_date']) { ?>
             <div class="alert alert-info text-center">This round has ended</div>
@@ -165,6 +231,7 @@
     <div class="col-6 col-md-2 col-lg-3 order-md-1 p-0 left-ads"><?= $settings['lottery_left_ad'] ?></div>
     <div class="col-6 col-md-2 col-lg-3 order-md-3 p-0 right-ads"><?= $settings['lottery_right_ad'] ?></div>
 </div>
+                        </div>
 <script>var _coinzilla_fp_id_ = "173623529710ebcf195",  _coinzilla_fp_interval_ = "5"; </script>
 <script src="https://coinzillatag.com/lib/fp.js"></script>
 <script async src="https://appsha-lon2.cointraffic.io/js/?wkey=lgXFfbiPoT"></script>
