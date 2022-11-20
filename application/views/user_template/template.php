d32bf0e1fd26e93eb40f6dcf248a023190f84988
ABDULLAH <abdullah@swishtag.com>
2022-11-20 18:03:14 +0500

    changes

--- application/views/user_template/template.php
+++ application/views/user_template/template.php
@@ -2,15 +2,6 @@
 <html lang="en">
 
 <head>
-
-    <meta charset="utf-8" />
-    <title><?= $page ?> | <?= $settings['name'] ?></title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="referrer" content="origin">
-    <meta content="<?= $settings['description'] ?>" name="description" />
-    <meta content="Vie Faucet Script" name="author" />
-
-    <!-- new code start -->
           <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
