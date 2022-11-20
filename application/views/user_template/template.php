ebd0c0361ae78ab65478c86530732910cb652de9
ABDULLAH <abdullah@swishtag.com>
2022-11-20 18:14:01 +0500

    changes

--- application/views/user_template/template.php
+++ application/views/user_template/template.php
@@ -629,6 +629,24 @@
   <script src="newAssets/js/plugins/choices.min.js"></script>
 
   <script>
+    var win = navigator.platform.indexOf('Win') > -1;
+    if (win && document.querySelector('#sidenav-scrollbar')) {
+      var options = {
+        damping: '0.5'
+      }
+      Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+    }
+  </script>
+  <!-- Github buttons -->
+  <script async defer src="https://buttons.github.io/buttons.js"></script>
+  <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
+  <script src="newAssets/js/soft-ui-dashboard.min.js?v=3"></script>
+
+  <script>
+    console.log("SCRIPT WORKING");
+  </script> 
+
+  <script>
     $(document).ready(function() {
       console.log("ready!");
     });
