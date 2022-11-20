aabee49af350ce03de90a57059824a13c3502413
ABDULLAH <abdullah@swishtag.com>
2022-11-21 01:27:16 +0500

    changes

--- application/views/user_template/withdraw.php
+++ application/views/user_template/withdraw.php
@@ -271,7 +271,7 @@
                       </div>
                       <small id="minimumWithdrawal" style="font-size: 12px;margin-top: -20px;"></small>
                     </div>
-                    <span style="background: #9999; padding: 15px; border-radius: 50%; width: 38px; height: 38px; margin-left: 6px;"><i class="fa-solid fa-right-left text-white" style="margin-top: 10px;"></i></span>
+                    <span style="background: #9999;border-radius: 50%; width: 38px; height: 38px; margin-left: 6px;"><i class="fa-solid fa-right-left text-white" style="margin-top: 10px;"></i></span>
                     <div class="" style="width: 43%;">
                       <div class="form-group">
                         <div class="input-group input-group-alternative mb-4">
