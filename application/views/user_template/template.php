e0b45902ffc0fada30fa02119d10a358ba5a706e
ABDULLAH <abdullah@swishtag.com>
2022-11-20 18:32:59 +0500

    changes

--- application/views/user_template/template.php
+++ application/views/user_template/template.php
@@ -658,7 +658,6 @@
 
   <!-- PREVIOUS CODES ######################################################################################################## -->
   
-  <script src="<?= base_url() ?>assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
   <script src="<?= base_url() ?>assets/libs/metismenu/metisMenu.min.js"></script>
   <script src="<?= base_url() ?>assets/libs/simplebar/simplebar.min.js"></script>
   <script src="<?= base_url() ?>assets/libs/node-waves/waves.min.js"></script>
