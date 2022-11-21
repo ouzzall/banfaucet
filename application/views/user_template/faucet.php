4cc16335f8df785d6712cca0d4e4b43212c2449e
massab <massab@swishtag.com>
2022-11-21 10:13:43 +0500

    changes

--- application/views/user_template/faucet.php
+++ application/views/user_template/faucet.php
@@ -76,15 +76,15 @@
 	<div class="col-lg-3 col-sm-6 mt-3">
 		<div class="card  mb-4">
 			<div class="card-body p-3">
-				<div class="row">
-					<div class="col-8">
+				<div class="" style="display: flex;justify-content: space-between;">
+					<div class="">
 						<div class="numbers">
 							<p class="text-sm mb-0 text-capitalize font-weight-bold">Reward</p>
-							<h5 class="font-weight-bolder mb-0" style="display: block ruby;">
+							<h5 class="font-weight-bolder mb-0" style="">
                             <?= currencyDisplay($settings['reward'], $settings) ?> <sup><span class="text-success me-2">+<?= $bonus ?>%</span></sup>
                         </h5> </div>
 					</div>
-					<div class="col-4 text-end">
+					<div class="">
 						<div class="icon icon-shape bg-gradient-primary shadow text-center border-radius-md"> <i class="fa-solid fa-gift text-lg opacity-10"></i> </div>
 					</div>
 				</div>
@@ -192,7 +192,7 @@
 <center><span id="ct_c1c1RkyZBbe"></span></center>
 <center><span id="ct_cJd95UwrCma"></span></center><p>
 <div class="row">
-    <div class="col-12 col-md-8 col-lg-6 order-md-2 mb-4 text-center">
+    <div class="col-12 col-md-8 mx-auto col-lg-6 order-md-2 mb-4 text-center">
         <?php
         if (isset($_SESSION['message'])) {
             echo $_SESSION['message'];
