10002bf9dfe55a5d1618ad4e0e2e369b19283454
massab <massab@swishtag.com>
2022-11-23 23:39:19 +0500

    changes

--- application/views/user_template/template.php
+++ application/views/user_template/template.php
@@ -680,14 +680,14 @@
 
   <!--    <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">-->
   <link rel="stylesheet" href="https://cdn.datatables.net/1.12.1/css/dataTables.bootstrap4.min.css">
-  <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
-  <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script>
+  <!-- <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap4.min.js"></script> -->
 
-  <script>
+  <!-- <script>
     $(document).ready(function() {
       $('#myTable').DataTable();
     });
-  </script>
+  </script> -->
 
   <?php if ($page == 'Dashboard') { ?>
     <script src="<?= base_url() ?>assets/libs/apexcharts/apexcharts.min.js"></script>
