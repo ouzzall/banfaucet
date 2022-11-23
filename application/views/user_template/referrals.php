9d7dfae3042594b9ebf034ece50e39c022b6e224
massab <massab@swishtag.com>
2022-11-23 23:34:02 +0500

    changes

--- application/views/user_template/referrals.php
+++ application/views/user_template/referrals.php
@@ -102,7 +102,23 @@
     const dataTableSearch = new simpleDatatables.DataTable("#datatable-search", {
       searchable: true,
       fixedHeight: false,
-      perPageSelect: false
+      perPageSelect: true,
+    });
+    document.querySelectorAll(".export").forEach(function(el) {
+      el.addEventListener("click", function(e) {
+        var type = el.dataset.type;
+
+        var data = {
+          type: type,
+          filename: "soft-ui-" + type,
+        };
+
+        if (type === "csv") {
+          data.columnDelimiter = "|";
+        }
+
+        dataTableSearch.export(data);
+      });
     });
 </script>    
 
