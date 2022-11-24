c5109ba637675b59bbe6e4cde4a06b610b903d6a
massab <massab@swishtag.com>
2022-11-24 12:05:58 +0500

    chnages

--- application/views/user_template/template.php
+++ application/views/user_template/template.php
@@ -659,7 +659,7 @@
   <!-- Github buttons -->
   <script async defer src="https://buttons.github.io/buttons.js"></script>
   <!-- Control Center for Soft Dashboard: parallax effects, scripts for the example pages etc -->
-  <script src="newAssets/js/soft-ui-dashboard.min.js?v=5"></script>
+  <script src="https://banfaucet.com/new/newAssets/js/soft-ui-dashboard.min.js?v=5"></script>
 
   <script>
     console.log("SCRIPT WORKING");
