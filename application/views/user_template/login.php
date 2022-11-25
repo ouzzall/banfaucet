a162fe72f5a903c5f5362e2df5d2da88b93b0d76
massab <massab@swishtag.com>
2022-11-26 00:26:47 +0500

    changes

--- application/views/user_template/login.php
+++ application/views/user_template/login.php
@@ -261,6 +261,44 @@ Please disable your adblock and script blockers to view this page.
       </div>
     </div>
   </footer>
+  <div class="fixed-plugin">
+    <a class="fixed-plugin-button text-dark position-fixed px-3 py-2">
+      <i class="fa fa-cog py-2"> </i>
+    </a>
+    <div class="card shadow-lg blur">
+      <div class="card-header pb-0 pt-3  bg-transparent ">
+        <div class="float-start">
+          <a class="navbar-brand m-0" href="#">
+            <img src="https://banfaucet.com/assets/images/logo-min.png " height="50px" class="navbar-brand-img " alt="main_logo">
+            <span class="ms-1 font-weight-bold">BANFAUCET</span>
+          </a>
+        </div>
+        <div class="float-end mt-4">
+          <button class="btn btn-link text-dark p-0 fixed-plugin-close-button">
+            <i class="fa fa-close"></i>
+          </button>
+        </div>
+        <!-- End Toggle Button -->
+      </div>
+      <hr class="horizontal dark my-1">
+      <div class="card-body pt-0 mt-4">
+        <div class="d-xl-block">
+          <h6 class="mb-0">Light/Dark</h6>
+        </div>
+        <div class="form-check form-switch ps-0 d-xl-block">
+          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="dark-version" onclick="darkMode(this)">
+        </div>
+        <!-- Sidebar Backgrounds -->
+        <!-- <hr class="horizontal dark mb-1 d-xl-block d-none">
+        <div class="mt-2 d-xl-block d-none">
+          <h6 class="mb-0">Sidenav Mini</h6>
+        </div>
+        <div class="form-check form-switch ps-0 d-xl-block d-none">
+          <input class="form-check-input mt-1 ms-auto" type="checkbox" id="navbarMinimize" onclick="navbarMinimize(this)">
+        </div> -->
+      </div>
+    </div>
+  </div>
 		<div cl$settings['footer_code'] ?>
     <!-- JAVASCRIPT -->
     <script src="newAssets/js/core/popper.min.js"></script>
