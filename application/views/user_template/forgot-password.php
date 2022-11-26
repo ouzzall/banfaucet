680ceba82eddce7a3fc8cfaffb713051f15473dd
massab <massab@swishtag.com>
2022-11-26 08:47:47 +0500

    changes

--- application/views/user_template/forgot-password.php
+++ application/views/user_template/forgot-password.php
@@ -177,11 +177,7 @@
                 <div class="text-center">
                   <button type="submit" class="btn bg-gradient-info w-100 my-4 mb-2">Reset Your Password</button>
                 </div>
-                <div class="mb-2 position-relative text-center">
-                  <p class="text-sm font-weight-bold mb-2 text-secondary text-border d-inline z-index-2 bg-white px-3">
-                    or
-                  </p>
-                </div>
+                <p style="text-align: center;margin-bottom: 0;">Or</p>
                 <div class="text-center">
                     <a href="<?= site_url('login') ?>">
                         <button type="button" class="btn bg-gradient-dark w-100 mt-2 mb-4">Sign in</button>
